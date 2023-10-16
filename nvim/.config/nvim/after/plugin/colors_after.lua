a437f2ec04f4698f5d64f45b66ccadf244b87ecc
Cameron Matsui <cameron.matsui@whatnot.com>
2023-10-16 12:52:07 -0400

    fix after

--- nvim/.config/nvim/after/plugin/colors_after.lua
+++ nvim/.config/nvim/after/plugin/colors_after.lua
@@ -1,2 +1,4 @@
 vim.cmd [[ hi NvimTreeNormal guibg=NONE ctermbg=NONE ]]
 vim.cmd [[ hi NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE ]]
+vim.cmd [[ hi VertSplit ctermbg=fg ]]
+vim.cmd [[ hi VertSplit guibg=bg guifg=bg ]]
