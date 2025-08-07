9849aaedf63c941ddf895f559feac21031407147
Cameron Matsui <cameron.matsui@whatnot.com>
2025-08-06 21:03:00 -0400

    everythikng but autocomplete :(

--- nvim/.config/nvim/lsp/ruff.lua
+++ nvim/.config/nvim/lsp/ruff.lua
@@ -1,7 +1,12 @@
 return {
     cmd = { "ruff", "server", "--preview" },
     filetypes = { "python" },
-    root_markers = { "pyproject.toml", "setup.py", ".git" },
+    root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        ".git",
+        "ruff.toml",
+    },
     single_file_support = true,
     settings = {},
 }
--- nvim/.config/nvim/lua/lib/autocmds.lua
+++ nvim/.config/nvim/lua/lib/autocmds.lua
@@ -71,3 +71,18 @@ end
 for _, pre_commit_config in ipairs(pre_commit_configs) do 
     setup_pre_commit(pre_commit_config)
 end
+
+-- lsp
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+    callback = function(event)
+        local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+
+        map("K", vim.lsp.buf.hover, "Hover Documentation")
+        map("gd", vim.lsp.buf.declaration, "Goto Declaration")
+        map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+        map("<leader>cr", vim.lsp.buf.rename, "Rename all references")
+    end,
+})
